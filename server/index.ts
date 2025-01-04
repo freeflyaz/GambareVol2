ad9e8de6320e32f14bb2bf01308f1f5cad694b82
Oweley <aleksandra.smolna@gmail.com>
2025-01-04 14:45:22 +0000

    server skeleton && typescript config

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+const app  = express();
+const port:number = 8000;
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, ()=>{
+  console.log(`server listens on ${port}`)
+})
+
+
+
