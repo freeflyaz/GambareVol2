474aa7b9591450cd91f9a087f42f5dee1bd4635b
Oweley <aleksandra.smolna@gmail.com>
2025-01-04 15:18:20 +0000

    successful husky hook test

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,11 @@
 import express from "express";
-import cors from "cors"
-const app  = express()
-const port:number = 8000
-app.use(express.json())
+import cors from "cors";
+const app  = express();
+const port = 8000;
+
+app.use(express.json());
 app.use(cors());
 
-
-//
-
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`server listens on ${port}`)
 })
-
-
-
