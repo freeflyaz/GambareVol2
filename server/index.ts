d4c0f848b0645d3eeb24d68d48ab57f4ccc54be3
Oweley <aleksandra.smolna@gmail.com>
2025-01-04 14:52:10 +0000

    Keep calm and commit

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import cors from "cors";
-const app  = express();
-const port:number = 8000;
-app.use(express.json());
+import cors from "cors"
+const app  = express()
+const port:number = 8000
+app.use(express.json())
 app.use(cors());
+
+
+//
 
 app.listen(port, ()=>{
   console.log(`server listens on ${port}`)
