f133ed249bd7cabe24bd9d7947b8c9ee0567a074
Oweley <aleksandra.smolna@gmail.com>
2025-01-06 11:45:58 +0000

    server => postman tested createTodo controller

---
+++ server/controller/controller.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+const prisma = new PrismaClient()
+
+async function createTodo(req: express.Request, res: express.Response) {
+  const {title, details } = req.body;
+  try {
+    let newTodo;
+    if (title && details) {
+      newTodo = await prisma.todo.create({
+       data : { title, details }
+     })
+    } else {
+      newTodo = await prisma.todo.create({
+        data: { title }
+      })
+    }
+    if (newTodo) {
+      res.status(200).send({msg: "new todo successfully created"})
+    } else {
+      res.status(400).send({ msg: "user error in createTodo"})
+    }
+  } catch (error) {
+    console.log("error in createTodo", error);
+    res.status(500).send({ msg: "server error in createTodo"})
+  }
+}
+
+export default {
+  createTodo
+}
--- server/router.ts
+++ server/router.ts
@@ -1,8 +1,11 @@
- // import express from "express";
+import express from "express";
+const router = express.Router();
+import Todo from "./controller/controller";
+// auth && protected routes && paid reports
+// gambare time planner - once weekly put through AI for compilation
+// free-tier & purged every 24 hours
+// lvl 1: create todo, update todo, show todos, show completed todos, clear all todos,
+router.post("/addTodo", Todo.createTodo);
+// lvl2: mark all todos as completed
 
- // const router = express.Router();
- // auth && protected routes && paid reports
- // gambare time planner - once weekly put through AI for compilation
- // free-tier & purged every 24 hours
- // lvl 1: create todo, update todo, show todos, show completed todos, clear all todos,
- // lvl2: mark all todos as completed
+export default router;
